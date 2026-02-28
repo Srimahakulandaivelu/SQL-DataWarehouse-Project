be774ef9aafac9eeed3c1378e28be7db972036be
Srimahakulandaivelu <168982069+Srimahakulandaivelu@users.noreply.github.com>
2026-02-28 13:11:50 +0530

    Rename database from DataWarehouse to Data_warehouse

--- scripts/init_datawarehouse.sql
+++ scripts/init_datawarehouse.sql
@@ -18,10 +18,10 @@ GO
 
 -- checking if there already exists a database named Data Warehouse
 -- if exists drop the db and recreate it
-IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'Data_warehouse')
 BEGIN
-    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
-    DROP DATABASE DataWarehouse;
+    ALTER DATABASE Data_warehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE Data_warehouse;
 END;
 GO
 
@@ -29,7 +29,7 @@ GO
 CREATE DATABASE Data_warehouse;
 GO
 
-USE DataWarehouse;
+USE Data_warehouse;
 GO
 
 -- Create Schemas
