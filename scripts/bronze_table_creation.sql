eee5daa0c81b3be093ad4f1cdcec6aaba51dd855
Srimahakulandaivelu <168982069+Srimahakulandaivelu@users.noreply.github.com>
2026-03-11 11:53:17 +0530

    Refactor bronze table creation SQL script
    Updated table creation script for bronze schema to ensure proper data types and drop existing tables if they exist.

--- scripts/bronze_table_creation.sql
+++ scripts/bronze_table_creation.sql
@@ -1,58 +1,90 @@
--- checking if the table alraedy exists in the database or not, 
--- if it does drop it and create new tables for the databases
+/*
+===============================================================================
+DDL Script: Create Bronze Tables
+===============================================================================
+Script Purpose:
+    This script creates tables in the 'bronze' schema, dropping existing tables 
+    if they already exist.
+	  Run this script to re-define the DDL structure of 'bronze' Tables
+===============================================================================
+*/
 
-drop table if exists bronze.crm_cust_info;
-create table bronze.crm_cust_info (
-cst_id int, 
-cst_key varchar(50),
-cst_firstname varchar(50),
-cst_lastname varchar(50),
-cst_marital_status varchar(50),
-cst_gndr varchar(50),
-cst_create_date date
-);
+IF OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_cust_info;
+GO
 
-drop table if exists bronze.crm_prd_info;
-create table bronze.crm_prd_info (
- prd_id int,
- prd_key varchar(50),
- prd_nm varchar(100),
- prd_cost int,
- prd_line varchar(50),
- prd_start_dt date, 
- prd_end_dt date
+CREATE TABLE bronze.crm_cust_info (
+    cst_id              INT,
+    cst_key             NVARCHAR(50),
+    cst_firstname       NVARCHAR(50),
+    cst_lastname        NVARCHAR(50),
+    cst_marital_status  NVARCHAR(50),
+    cst_gndr            NVARCHAR(50),
+    cst_create_date     DATE
 );
+GO
 
-drop table if exists bronze.crm_sales_details;
-create table bronze.crm_sales_details(
-	sls_ord_num int,
-    sls_prd_key varchar(50),
-    sls_cust_id varchar(50),
-    sls_order_dt varchar(50),
-    sls_ship_dt varchar(50),
-    sls_due_dt varchar(50),
-    sls_sales int,
-    sls_quantity int,
-    sls_price int
-);
+IF OBJECT_ID('bronze.crm_prd_info', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_prd_info;
+GO
 
-drop table if exists bronze.erp_cust_az12;
-create table bronze.erp_CUST_AZ12 (
-	CID varchar(50),
-    BDATE date,
-    GEN varchar(50)
+CREATE TABLE bronze.crm_prd_info (
+    prd_id       INT,
+    prd_key      NVARCHAR(50),
+    prd_nm       NVARCHAR(50),
+    prd_cost     INT,
+    prd_line     NVARCHAR(50),
+    prd_start_dt DATETIME,
+    prd_end_dt   DATETIME
 );
+GO
 
-drop table if exists bronze.erp_loc_a101;
-create table bronze.erp_LOC_A101 (
-	CID varchar(50),
-    CNTRY varchar(50)
-);
+IF OBJECT_ID('bronze.crm_sales_details', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_sales_details;
+GO
 
-drop table if exists bronze.erp_px_cat_g1v2;
-create table bronze.erp_PX_CAT_G1V2(
-  ID varchar(50),
-  CAT varchar(50),
-  SUBCAT varchar(50),
-  MAINTENANCE varchar(50)
+CREATE TABLE bronze.crm_sales_details (
+    sls_ord_num  NVARCHAR(50),
+    sls_prd_key  NVARCHAR(50),
+    sls_cust_id  INT,
+    sls_order_dt INT,
+    sls_ship_dt  INT,
+    sls_due_dt   INT,
+    sls_sales    INT,
+    sls_quantity INT,
+    sls_price    INT
 );
+GO
+
+IF OBJECT_ID('bronze.erp_loc_a101', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_loc_a101;
+GO
+
+CREATE TABLE bronze.erp_loc_a101 (
+    cid    NVARCHAR(50),
+    cntry  NVARCHAR(50)
+);
+GO
+
+IF OBJECT_ID('bronze.erp_cust_az12', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_cust_az12;
+GO
+
+CREATE TABLE bronze.erp_cust_az12 (
+    cid    NVARCHAR(50),
+    bdate  DATE,
+    gen    NVARCHAR(50)
+);
+GO
+
+IF OBJECT_ID('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_px_cat_g1v2;
+GO
+
+CREATE TABLE bronze.erp_px_cat_g1v2 (
+    id           NVARCHAR(50),
+    cat          NVARCHAR(50),
+    subcat       NVARCHAR(50),
+    maintenance  NVARCHAR(50)
+);
+GO
